1ca9ed88077dc2395e931c5834d5c322f1ddf20c
unknown <jdchuquispuma@gmail.com>
2022-06-15 09:42:27 -0500

    Añadiendo un método constructor

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Anahí Salgado");
 
         myDoctor.name = "Jean Chuquispuma";
         myDoctor.showName();
